09bad8074f29b0a54cd6b0acc920bd2d2dd08f54
dwarjie <mozencode@gmail.com>
2020-06-02 21:35:24 +0800

    Added the hamburger animation and icon menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+
+// reference classes
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
 	module.hot.accept();
